d991671ae5460f548995225a4aba1723cef89b0a
Asemyasser <asemyasser42@gmail.com>
2023-09-18 00:59:43 +0300

    Updates

---
+++ static/js/143.eb8b7c7d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktask1=self.webpackChunktask1||[]).push([[143],{951:function(e,a,t){t.r(a),t.d(a,{default:function(){return r}});t(791);var s=t.p+"static/media/error.c48f47d264dc1e8a121f.jpg",c=t(874),n=t(184);function r(){return(0,n.jsxs)("div",{className:"container text-center",children:[(0,n.jsx)("h1",{children:"Not Found"}),(0,n.jsx)(c.LazyLoadImage,{src:s,className:"w-50 ",alt:""})]})}}}]);
+//# sourceMappingURL=143.eb8b7c7d.chunk.js.map
